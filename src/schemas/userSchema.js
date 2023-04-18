3a79201ac060ed9ac529f4cb7cb5ab28b4c87ff6
Igor Guimarães <igorgui.tec@gmail.com>
2023-04-18 17:06:18 -0300

    Manutenção: Remoção de confirmPassword

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -4,6 +4,5 @@ const minPass = 3;
 export const userSchema = joi.object({
     name: joi.string().min(3).required(),
     password: joi.string().min(minPass).required(),
-    confirmPassword: joi.string().min(minPass).required(),
     email: joi.string().email().min(minPass)
 })
